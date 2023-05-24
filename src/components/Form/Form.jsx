541fb967ca87814ab51289d6152b7723a59d735c
KseniaLichnova <Lichnova1@mail.ru>
2023-05-24 19:53:27 +0200

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -17,7 +17,7 @@ const Form = () => {
         }
         tg.sendData(JSON.stringify(data));
 
-    }, [])
+    }, [country, street, subject])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
@@ -25,7 +25,7 @@ const Form = () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
 
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
