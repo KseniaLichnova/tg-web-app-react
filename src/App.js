ecb3f48da3cb3f7f8d7549633331b9358f5e2e3a
KseniaLichnova <Lichnova1@mail.ru>
2023-05-24 14:54:11 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useEffect} from "react";
-const tg = window.Telegram.WebApp;
+
 
 function App() {
 
@@ -9,13 +9,11 @@ function App() {
 
     },[])
 
-    const onClose = () => {
-        tg.close()
-    }
+
   return (
     <div className="App">
       work
-        <button onClick={onClose}>Закрыть</button>
+
 
     </div>
   );
