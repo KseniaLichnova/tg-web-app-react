f9a15b24c5d2351eb46822a44d36c020702ce5b2
KseniaLichnova <Lichnova1@mail.ru>
2023-05-26 18:29:17 +0200

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from "../Button/Button";
 import './ProductItem.css'
+import bighit from "../ProductItem/bighit.png";
 
 const ProductItem = ({product, className, onAdd}) => {
     const onAddHandler = () => {
@@ -9,7 +10,9 @@ const ProductItem = ({product, className, onAdd}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}>{product.img}</div>
+            <div className={'img'}>
+            <img src= {"https://avatars.dzeninfra.ru/get-zen_doc/1661842/pub_63aca467a2d92211f5fa822d_63aca9910286c861c5a68205/scale_1200"}  class={"img"} />
+        </div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
